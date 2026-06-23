d1230c2876fa60f4735b0684ef6de28bc76c951b
sakxi-77 <sakshilndh@gmail.com>
2026-06-23 16:19:11 +0530

    Fix port for Render deployment

--- init.js
+++ init.js
@@ -679,7 +679,7 @@ app.get('/profile', (req, res) => {
 
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);

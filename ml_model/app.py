fc1dfe23fdaa9b9806889d1f732729172f3ec3db
sakxi-77 <sakshilndh@gmail.com>
2026-06-27 14:13:35 +0530

    Configure Flask for Render

--- ml_model/app.py
+++ ml_model/app.py
@@ -65,6 +65,8 @@ def predict():
 
 
 # ================= RUN SERVER =================
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
